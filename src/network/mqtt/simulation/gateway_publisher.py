4064504bc406d80c76edbadc38d105a6695dc155
golam sobhani chowdhury <golamsobhani.chowdhury@hm.com>
2024-01-08 10:20:55 +0100

    ddd

--- src/network/mqtt/simulation/gateway_publisher.py
+++ src/network/mqtt/simulation/gateway_publisher.py
@@ -6,13 +6,19 @@ class PublisherGateway(MQTTClient):
     def __init__(self, client_id, broker_address):
         super().__init__(client_id=client_id, broker_address=broker_address)        
         self.topic = f"/{client_id}"
+        self.client.on_publish = self.on_publish
 
     def publish_message(self, message):
         self.client.publish(self.topic, message)
+        self.message = message
         # print(f"Published message: {message}")
+
+    def on_publish(self, client, userdata, mid):
         self.log_message(
             event_type=EVENT_MSG_SENT,
             from_device=self.client_id,
             to_device="",
-            message=message,
+            message=self.message,
         )
+       
+
